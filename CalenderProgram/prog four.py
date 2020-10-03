9a3e186ed9cd59ac1d6471ab6b5065f292ff4051
Ruhikaa <ruhikaakpannem@gmail.com>
2020-10-03 12:58:52 -0700

    My calender program

---
+++ CalenderProgram/prog four.py
@@ -0,0 +1,13 @@
+mycalender1 = '''Day:      Time:              Subject:
+Monday-   9:15-10:15  AM     Science
+          10:35-11:40 AM     Math    
+          12:10-1:15  PM     PF  
+Tuesday-  9:15-10:15  AM     Woodshop
+          10:35-11:40 AM     ELA
+          12:10-1:15  PM     Social Studies
+(Breaks in between)'''
+print(len(mycalender1))
+if 'Monday' in mycalender1:
+    print("Monday is in mycalender1")
+else:
+    print("Monday is not in mycalender1")
